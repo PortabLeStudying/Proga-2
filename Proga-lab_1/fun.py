6562650283a51ccc8ae21ecdb7f727919da4764b
PortabLeStudying <abentelev@gmail.com>
2025-10-01 14:09:55 +0300

    Add files via upload

---
+++ Proga-lab_1/fun.py
@@ -0,0 +1,7 @@
+def two_sum(nums, tar):
+    num_x = {}
+    for i, num in enumerate(nums):
+        comp = tar - num
+        if comp in num_x:
+            return [num_x[comp], i]
+        num_x[num] = i
---
+++ Proga-lab_1/test.py
@@ -0,0 +1,15 @@
+import unittest
+from fun import two_sum
+
+class TestTwoSum(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(sorted(two_sum([2,7,11,15], 9)), [0,1])
+
+    def test_2(self):
+        self.assertEqual(sorted(two_sum([3,2,4], 6)), [1,2])
+
+    def test_3(self):
+        self.assertEqual(sorted(two_sum([3,3], 6)), [0,1])
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
